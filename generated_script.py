148287a7e18a39c8b16e5b8ae7cc30529ad44da5
aoiastro <ninjin0925@icloud.com>
2026-03-14 23:47:51 +0900

    TempuraPocket: Add generated generated_script.py

--- generated_script.py
+++ generated_script.py
@@ -1,23 +1,6 @@
-#!/usr/bin/env python3
+import itertools
 
-fruits = ['apple', 'cherry', 'date', 'banana', 'elderberry']
+words = ['apple', 'cherry', 'date', 'banana', 'elderberry']
 
-def generate_permutations():
-    permutations = []
-    used = [False] * len(fruits)
-    def backtrack(start, path):
-        if len(path) == len(fruits):
-            permutations.append(path.copy())
-            return
-        for i in range(len(fruits)):
-            if not used[i]:
-                used[i] = True
-                path.append(fruits[i])
-                backtrack(i + 1, path)
-                path.pop()
-                used[i] = False
-    backtrack(0, [])
-    return permutations
-
-for perm in generate_permutations():
+for perm in itertools.permutations(words):
     print(' '.join(perm))
