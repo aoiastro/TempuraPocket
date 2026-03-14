205ecdffd933d17dce0eca9862646519ba6bfa92
aoiastro <ninjin0925@icloud.com>
2026-03-14 23:39:03 +0900

    TempuraPocket: Add generated generated_script.py

--- generated_script.py
+++ generated_script.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
-import random
 
-# Generate a random poem by selecting words from a predefined list
-words = ["apple", "banana", "cherry", "date", "elderberry"]
-poem = []
-for i in range(5):
-    line = " ".join(random.choices(words, k=5))
-    poem.append(line)
-print("\n".join(poem))
+fruits = ['apple', 'cherry', 'date', 'banana', 'elderberry']
+
+def generate_permutations():
+    permutations = []
+    used = [False] * len(fruits)
+    def backtrack(start, path):
+        if len(path) == len(fruits):
+            permutations.append(path.copy())
+            return
+        for i in range(len(fruits)):
+            if not used[i]:
+                used[i] = True
+                path.append(fruits[i])
+                backtrack(i + 1, path)
+                path.pop()
+                used[i] = False
+    backtrack(0, [])
+    return permutations
+
+for perm in generate_permutations():
+    print(' '.join(perm))
